b2549e0906dbad313c794a240ed3aee416eb0ec4
Emmanuel I. Obi <withtwoemms@gmail.com>
2015-08-16 09:58:10 -0500

    Fix typo

--- app/controllers/scavenger_hunts_controller.rb
+++ app/controllers/scavenger_hunts_controller.rb
@@ -1,5 +1,6 @@
 class ScavengerHuntsController < ApplicationController
 	def index
-		render_template file: '#{ Rails.root }public/position-html'
+		# render_template file: '#{ Rails.root }public/position-html'
+		render file: 'public/position-test.html'
 	end
 end
