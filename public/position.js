ec75955aab113f4e53c91e3f93721668290717fe
Emmanuel I. Obi <withtwoemms@gmail.com>
2015-08-16 11:02:18 -0500

    Deliver JS object w/ coordinats using .watchPosition()

--- public/position.js
+++ public/position.js
@@ -1,6 +1,31 @@
-coords = {
-    lat: 0,
-    lon: 0
+// coords = {
+//     lat: 0,
+//     lon: 0
+// }
+
+// options = {
+//   enableHighAccuracy: false,
+//   timeout: 5000,
+//   maximumAge: 0
+// };
+
+// var lat, lon;
+// var currentLocation = function() { 
+//     navigator.geolocation.getCurrentPosition(function(position) { 
+//         lat = position.coords.latitude; 
+//         lon = position.coords.longitude; 
+//         console.log(lat, lon);
+//     }, null, options);
+    
+//    coords.lat = lat; 
+//    coords.lon = lon; 
+
+//    return coords 
+// };
+
+ coords = {
+  lat: 0,
+  lon: 0
 }
 
 options = {
@@ -10,25 +35,51 @@ options = {
 };
 
 var lat, lon;
-var currentLocation = function() { 
-    navigator.geolocation.getCurrentPosition(function(position) { 
-        lat = position.coords.latitude; 
-        lon = position.coords.longitude; 
-        console.log(lat, lon);
-    }, null, options);
+// var currentLocation = function() { 
+//     navigator.geolocation.getCurrentPosition(function(position) { 
+//         lat = position.coords.latitude; 
+//         lon = position.coords.longitude; 
+//         console.log(lat, lon);
+//     }, null, options);
     
-   coords.lat = lat; 
-   coords.lon = lon; 
+//    coords.lat = lat; 
+//    coords.lon = lon; 
 
-   return coords 
-};
+//    return coords 
+// };
 
-    function watchLocation() { 
+var x = document.getElementById("demo");
+
+function getLocation() {
+  if (navigator.geolocation) {
       navigator.geolocation.watchPosition(currentLocation);
-    }
-$(document).ready( function() {
-    // currentLocation();
-    // setTimeout(console.log("lat"), 3000);
-    // console.log(currentLocation());
-    watchLocation();
-});
+      // console.log(currentLocation());
+      navigator.geolocation.watchPosition(showPosition);
+      // navigator.geolocation.watchPosition(function(coords) {
+      //   lat = position.coords.latitude; 
+      //   lon = position.coords.longitude;
+      //   return lat
+      // });
+      // navigator.geolocation.watchPosition(showPosition);
+      // navigator.geolocation.watchPosition(showPosition);
+  } else { 
+      x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+    
+function showPosition(position) {
+    x.innerHTML="Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude;  
+}
+
+function currentLocation(position) { 
+  lat = position.coords.latitude; 
+  lon = position.coords.longitude; 
+  console.log(lat, lon);
+    
+  coords.lat = lat; 
+  coords.lon = lon; 
+  console.log(coords)
+
+  return coords 
+};
